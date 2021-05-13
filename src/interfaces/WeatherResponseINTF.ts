7c84e9e3a0463505f2f0e6b7ec748594fd65d480
Rinu Yatim <rinuyatim@gmail.com>
2021-05-14 01:51:15 +0200

    Initial dist subtree commit

--- src/interfaces/WeatherResponseINTF.ts
+++
@@ -1,9 +0,0 @@
-export default interface WeatherResponseINTF {
-    date?: number | null,
-    condition?: string | null,
-    nowTemp?: number | null,
-    dayTemp?: number | null,
-    minTemp?: number | null,
-    maxTemp?: number | null,
-    icon?: string | null
-}
